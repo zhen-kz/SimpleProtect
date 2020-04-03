8199b81da2d07d686094eeb462cd6fbce0a3142e
paang <2837839125@qq.com>
2020-04-04 00:48:42 +0800

    完成一个客户端和一个(2, 3)IS群的模拟

--- src/com/witcher/IdentifyServer.java
+++ src/com/witcher/IdentifyServer.java
@@ -1,5 +1,216 @@
 package com.witcher;
 
-public class IdentifyServer
-{
+import function.ClientCryptoTools;
+import function.CryptographicAlgorithm;
+import function.IdentifyServerCryptoTools;
+import it.unisa.dia.gas.jpbc.Element;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class IdentifyServer {
+    private IdentifyServerCryptoTools tools;
+    private int t = 2;
+    private int n = 3;
+    private Element[] ki;
+    private final int MAX_PHI = 5;
+    private final int MAX_RHO = 5;
+    //Φ
+    private int phi;
+    //ρ
+    private int rho;
+    private Element[] r_i;
+    private byte[][] sp_i;
+
+    public IdentifyServer() {
+        tools = new IdentifyServerCryptoTools("a.properties", "P.properties");
+    }
+
+    public IdentifyServer(int t, int n) {
+        tools = new IdentifyServerCryptoTools("a.properties", "P.properties");
+        this.t = t;
+        this.n = n;
+    }
+
+    private Element[] PKi;
+
+    public HashMap<String, Element[]> MasterKeyGen() {
+        Element[][] fx = new Element[n][];
+        Element[][] aP = new Element[n][];
+        Element[][] fji = new Element[n][n];
+        for (int i = 0; i < n; ++i) {
+            fx[i] = tools.polynomialFx(t - 1);
+            aP[i] = tools.Compute_aPs(fx[i]);
+        }
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+//                if (i == j) continue;
+                fji[i][j] = tools.ComputeFx(fx[j], i + 1);
+            }
+        }
+        //check
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (i == j) continue;
+                System.out.printf("IS%d: verifying IS%d ...  :", i + 1, j + 1);
+                System.out.println(tools.CheckFji_aP(fji[i][j], aP[j], i + 1));
+            }
+        }
+        Element[] ki = new Element[n];
+        Element[] PKi = new Element[n], a0P = new Element[n];
+        for (int i = 0; i < n; ++i) {
+            ki[i] = tools.ComputeKi(fji[i]);
+            PKi[i] = tools.ComputePKi(ki[i]);
+            a0P[i] = aP[i][0];
+        }
+        /*-------test-------*/
+        ClientCryptoTools ctools = new ClientCryptoTools("a.properties", "P.properties");
+        Element psw = ctools.H("hello world".getBytes()).powZn(ctools.getRandomElementInZpPlus()).getImmutable();
+        for (int i = 0; i < n; ++i) {
+            System.out.printf("(k%d, PK%d): ", i, i);
+            System.out.println(ctools.CheckE(psw.powZn(ki[i]), tools.getGGeneratorP(),
+                    psw, PKi[i]));
+            System.out.println("ki: " + ki[i]);
+            System.out.println("PKi: " + PKi[i]);
+        }
+        /*-------test end-------*/
+        Element[] PK = new Element[1];
+        PK[0] = tools.ComputePK(a0P);
+        HashMap<String, Element[]> map = new HashMap<String, Element[]>();
+        this.ki = ki;
+        this.PKi = PKi;
+        map.put("PKi", PKi);
+        map.put("PK", PK);
+        return map;
+    }
+
+    //σ
+    public Element[] ComputeSigma_i(Element psw) {
+        if (ki == null || psw == null) return null;
+        Element[] sigma_i = new Element[ki.length];
+        for (int i = 0; i < ki.length; ++i) {
+            sigma_i[i] = psw.duplicate().powZn(ki[i].duplicate()).getImmutable();
+        }
+        /*-------test-------*/
+        ClientCryptoTools client = new ClientCryptoTools("a.properties", "P.properties");
+        for (int i = 0; i < ki.length; ++i) {
+            System.out.printf("sigma_%d checking: ", i);
+            System.out.println(client.CheckE(sigma_i[i], client.getGGeneratorP(), psw, PKi[i]));
+            System.out.println("psw: " + psw);
+            System.out.println("ki: " + ki[i]);
+            System.out.println("PKi: " + PKi[i]);
+        }
+        /*-------test end-------*/
+        return sigma_i;
+    }
+
+    public void SetSpi(byte[][] sp_i) {
+        if (sp_i == null) return;
+        this.sp_i = sp_i.clone();
+        phi = 0;
+        rho = 0;
+    }
+
+    public boolean CheckPhiRhoAndIncreaseRho() {
+        if (rho > MAX_RHO || phi > MAX_PHI) {
+            return false;
+        }
+        rho++;
+        return true;
+    }
+
+    public HashMap<String, Element[]> ComputeSigma_iAndRi(Element psw) {
+        Element[] sigma_i = this.ComputeSigma_i(psw);
+        Element[] r_i = new Element[n];
+        for (int i = 0; i < n; ++i) {
+            r_i[i] = tools.getRandomElementInZpPlus();
+        }
+        HashMap<String, Element[]> res = new HashMap<>();
+        res.put("sigma_i", sigma_i);
+        res.put("r_i", r_i);
+        this.r_i = r_i;
+        return res;
+    }
+
+    public Element[] DecryptRi(byte[][] cipher, int bytesLen) {
+        if (cipher == null) return null;
+        Element[] ri = new Element[cipher.length];
+        for (int i = 0; i < cipher.length; ++i) {
+            ri[i] = tools.getElementFromBytesInZp(
+                    CryptographicAlgorithm.SM4_Decrypt_ECB(sp_i[i], cipher[i], bytesLen));
+        }
+        return ri;
+    }
+
+    public boolean CheckRi(Element[] r) {
+        if (r == null || r_i == null || r.length != r_i.length) return false;
+        boolean res = true, temp;
+        for (int i = 0; i < r.length; ++i) {
+            temp = r[i].isEqual(r_i[i]);
+            System.out.printf("r%d checking:  " + temp + "\n", i + 1);
+            res &= temp;
+        }
+        return res;
+    }
+
+    public Element[] ComputeAUTi(String token) {
+        Element[] res = new Element[n];
+        for (int i = 0; i < n; ++i) {
+            res[i] = tools.ComputeAUTi(ki[i], token.getBytes());
+        }
+        return res;
+    }
+
+    public byte[][] EncryptToken(Element[] aut, String token) {
+        if (aut == null || token == null) return null;
+        byte[][] res = new byte[aut.length][];
+        byte[] token_bytes = token.getBytes().clone();
+        final int aut_len = aut[0].toBytes().length;
+        System.out.println("Aut length:  " + aut_len);
+        byte[] block = new byte[aut_len + token_bytes.length];
+        System.arraycopy(token_bytes, 0, block, aut_len, token_bytes.length);
+        for (int i = 0; i < aut.length; ++i) {
+            System.arraycopy(aut[i].toBytes(), 0, block, 0, aut_len);
+            res[i] = CryptographicAlgorithm.SM4_Encrypt_ECB(sp_i[i], block);
+        }
+        return res;
+    }
+
+    public Element[] RenewShare() {
+        Element[][] lx = new Element[n][];
+        Element[][] bP = new Element[n][];
+        for (int i = 0; i < n; ++i) {
+            lx[i] = tools.polynomialLx(t - 1);
+            bP[i] = tools.Compute_aPs(lx[i]);
+        }
+
+        Element[][] lij = new Element[n][n];
+//        | l1(1)  l2(1) l3(1)|
+//        | l1(2)  l2(2) l3(2)|
+//        | l1(3)  l2(3) l3(3)|
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+//                if (i == j) continue;
+                lij[j][i] = tools.ComputeLx(lx[i], j + 1);
+            }
+        }
+
+        //检查
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (i == j) continue;
+                System.out.printf("IS%d: verifying IS%d ...  :", i + 1, j + 1);
+                System.out.println(tools.CheckLji_bP(lij[i][j], bP[j], i + 1));
+            }
+        }
+        Element[] PKi = new Element[ki.length];
+        for (int i = 0; i < ki.length; ++i) {
+            ki[i] = tools.RenewKi(ki[i], lij[i]);
+            PKi[i] = tools.ComputePKi(ki[i]);
+        }
+        this.PKi = PKi;
+        return PKi;
+    }
+
 }
--- src/com/witcher/Main.java
+++ src/com/witcher/Main.java
@@ -4,30 +4,137 @@ import it.unisa.dia.gas.jpbc.*;
 import function.*;
 import org.bouncycastle.crypto.digests.SM3Digest;
 import org.bouncycastle.util.encoders.Hex;
+import it.unisa.dia.gas.plaf.jpbc.pairing.PairingFactory;
+import it.unisa.dia.gas.plaf.jpbc.pairing.a.TypeACurveGenerator;
 
-import java.io.IOException;
+import java.io.*;
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class Main {
+    private IdentifyServer[] IS;
 
-    public static void main(String[] args){
-        /* test SM4*/
-        byte[] str = {0x1, 0x2, 0x3};
-        byte[] key = new byte[16];
-        Arrays.fill(key, (byte)12);
-        byte[] cipher = CryptographicAlgorithm.SM4_Encrypt_ECB(key, str);
-        System.out.println(new String(Hex.encode(cipher)));
-        byte[] str_ = CryptographicAlgorithm.SM4_Decrypt_ECB(key, cipher);
-        System.out.println(new String(Hex.encode(str_)));
-//        System.out.println("\n" + SM3.byteArrayToHexString(SM3.hash(test.getBytes())));
-//        System.out.println("\nhash end");
-//        SM3Digest sm3 = new SM3Digest();
-//        byte[] msg1 = test.getBytes(), md = new byte[32];
-//        sm3.update(msg1, 0, msg1.length);
-//        sm3.doFinal(md, 0);
-//        String s = new String(Hex.encode(md));
-//        System.out.println(s.toUpperCase());
-        //System.out.println(CryptographicAlgorithm.SM3("abc".getBytes()));
-        //CryptoTools.test();
+    public IdentifyServer[] getISList() {
+        return IS;
+    }
+
+    public static void main(String[] args) throws Exception {
+//        ClientCryptoTools client = new ClientCryptoTools("a.properties", "P.properties");
+//        IdentifyServerCryptoTools is = new IdentifyServerCryptoTools("a.properties", "P.properties");
+//        CryptoTools.test();
+        final int t = 2, n = 3;
+        Client user = new Client();
+        IdentifyServer IS = new IdentifyServer(t, n);
+//        if (user.test())
+//            return;
+        /*variable*/
+
+        boolean status;
+        String password = "hello world";
+        Element psw, sigma_u;
+        HashMap<String, Element[]> PKs;
+        Element[] sigma_i;
+        byte[] sp_u;
+        byte[][] sp_i;
+
+
+
+        /*-------------register-------------*/
+
+
+        //客户端混淆密码
+        psw = user.ConfusePassword(password);
+        //IS产生ki, PKi, PK
+        PKs = IS.MasterKeyGen();
+        Element[] PKi = PKs.get("PKi");
+        Element PK = PKs.get("PK")[0];
+        //IS计算σi
+        sigma_i = IS.ComputeSigma_i(psw);
+        //客户端检查σi
+        user.CheckSigmaI(sigma_i, PKi);
+        //客户端计算σu
+        Element[] sigma_i_buffer = new Element[t];
+        System.arraycopy(sigma_i, 0, sigma_i_buffer, 0, t);
+        sigma_u = user.ComputeSigmaU(sigma_i_buffer, new int[]{1, 2});
+        //检查σu
+        user.CheckSigmaU(sigma_u, PK);
+        //计算spu, spi
+        sp_u = user.ComputeSpu(sigma_u);
+        System.out.println("sp_u: " + new String(Hex.encode(sp_u)));
+        sp_i = user.ComputeSpi(sp_u, new int[]{1, 2, 3});
+        //IS保存spi
+        IS.SetSpi(sp_i);
+
+
+
+        /*-------------sign-on-------------*/
+
+
+        /*RetriSerPsw*/
+        //客户端应在这个阶段获取PKi和PK, 但为了方便继续使用register阶段的变量
+
+        //客户端混淆密码
+        psw = user.ConfusePassword("hello world");
+        //IS检查ρ
+        if (!IS.CheckPhiRhoAndIncreaseRho()) {
+            System.out.println("ρ > MAX_ρ");
+        }
+        //计算{σi, ri}
+        HashMap<String, Element[]> sigma_i_Ri = IS.ComputeSigma_iAndRi(psw);
+        sigma_i = sigma_i_Ri.get("sigma_i");
+        Element[] r_i = sigma_i_Ri.get("r_i");
+        //客户端检查σi是否正确
+        user.CheckSigmaI(sigma_i, PKi);
+        //计算σu
+        System.arraycopy(sigma_i, 0, sigma_i_buffer, 0, t);
+        sigma_u = user.ComputeSigmaU(sigma_i_buffer, new int[]{1, 2});
+        //检查σu是否正确
+        user.CheckSigmaU(sigma_u, PK);
+
+
+        //Authentication
+
+        //客户端计算spu, spi
+        sp_u = user.ComputeSpu(sigma_u);
+        sp_i = user.ComputeSpi(sp_u, new int[]{1, 2, 3});
+        //客户端用spi加密IS的随机数ri
+        byte[][] Esp_i = user.EncryptRi(sp_i, r_i);
+        //IS解密
+        Element[] r = IS.DecryptRi(Esp_i, r_i[0].getLengthInBytes());
+        //检查随机数是否正确
+        IS.CheckRi(r);
+        //IS计算aut_i
+        final String Token = "uestc";
+        Element[] aut_i = IS.ComputeAUTi(Token);
+        //将{aut_i||Token}用sp_i加密
+        byte[][] EAutToken_i = IS.EncryptToken(aut_i, Token);
+        //客户端解密
+        final int bytes_len = aut_i[0].getLengthInBytes() + Token.getBytes().length;
+        HashMap<String, byte[][]> de = user.DecryptToken(sp_i, EAutToken_i, bytes_len);
+        aut_i = user.Aut2Element(de.get("aut_i"));
+        System.out.println("Token bytes:       " + new String(Hex.encode(Token.getBytes())));
+        System.out.println("user_Token bytes:  " + new String(Hex.encode(de.get("token")[0])));
+        //检查aut_i与Token是否正确
+        user.CheckAutToken(aut_i, de.get("token")[0], PKi);
+        //计算Aut_u
+        Element aut_u = user.ComputeAutU(aut_i, new int[]{1, 2, 3});
+        String user_Token = new String(de.get("token")[0]);
+        System.out.println("user_Token:  " + user_Token);
+        //检查Aut_u是否正确
+        status = user.CheckAutU(aut_u, user_Token.getBytes(), PK);
+        System.out.println("Aut_u checking:  " + status);
+
+
+        /*-------------RenewShare-------------*/
+
+
+        PKi = IS.RenewShare();
+        sigma_i = IS.ComputeSigma_i(psw);
+        user.CheckSigmaI(sigma_i, PKi);
+        sigma_u = user.ComputeSigmaU(sigma_i, new int[]{1,2,3});
+        user.CheckSigmaU(sigma_u, PK);
+        for (int i = 0; i < PKi.length; ++i) {
+            System.out.printf("new PK%d:  " + PKi[i] + "\n", i + 1);
+        }
     }
 }
